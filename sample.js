ada66687cd1e3a355334772d2cb3dea1c481f5bd
Sam-O-Parker <45101960+Sam-O-Parker@users.noreply.github.com>
2018-11-16 16:17:18 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -23,7 +23,7 @@ var s3 = new AWS.S3();
 
 // Create a bucket and upload something into it
 var bucketName = 'node-sdk-sample-' + uuid.v4();
-var keyName = 'hello_world.txt';
+var keyName = uuid.v4() + 'hello_world.txt';
 
 s3.createBucket({Bucket: bucketName}, function() {
   var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
