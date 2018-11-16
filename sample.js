78d39151d3590091c8f9708b3044dbfbc2ca0507
Sam-O-Parker <45101960+Sam-O-Parker@users.noreply.github.com>
2018-11-16 16:40:45 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -38,5 +38,6 @@ s3.createBucket({Bucket: bucketName}, function() {
  s3.listObjects(params, function(err, data) {
    if (err) console.log(err, err.stack); // an error occurred
    else     console.log(data);
+   });
   });
 });
