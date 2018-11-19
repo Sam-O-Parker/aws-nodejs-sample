61d2f28bf459931c0f16fa4651c9df48859d1b54
Sam-O-Parker <45101960+Sam-O-Parker@users.noreply.github.com>
2018-11-19 17:37:52 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -29,10 +29,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+console.log('yo');
+
 app.get('/', (request, response) => {
   response.json(s3.listObjects(params, function(err, data) {
    if (err) console.log(err, err.stack); // an error occurred
-   else     print(data);
+   else     console.log(data);
    })
   )
 })
